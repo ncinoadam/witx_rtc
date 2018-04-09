71979ef2fc1da43c0c90a1fb378fb398bd4935f2
Adam Larson <adam.larson@ncino.com>
2018-04-09 11:33:33 -0400

    Endpoint for clearing chat

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -23,6 +23,18 @@ module.exports = {
             Chat.watch(req.socket);
             console.log("New User Subscribed to: " + req.socket.id);
         }
+    },
+
+    /**
+     * Clear Chat api
+     * In production this should be wrapped with a permission check
+     * via policies. Also even though this demo doesn't have them
+     * All of these endpoints should be covered with Unit tests.
+     */
+    clearChat: function(req, res) {
+        Chat.destroy().exec((err, chats) => {
+            res.json(chats);
+        })
     }
 };
 
